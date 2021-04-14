79a8ddb345705706811c6825928503f74c4a3ed3
MrTaiko314 <glucio314@gmail.com>
2021-04-13 23:16:48 -0300

    Corrigido bug no tratamento do json

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ async def on_message(message):
         mensagem_do_usuario = message.content
         msgformatada = mensagem_do_usuario[6:]
         url = 'https://query1.finance.yahoo.com/v8/finance/chart/' + msgformatada + '.SA?region=US&lang=en-US&includePrePost=false&interval=2m&useYfid=true&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance'
-        data = json.loads(requests.get(url))
+        data = requests.get(url).json()
         preco_atual = float(data['chart']['result'][0]['meta']['regularMarketPrice'])
         ultimo_fechamento = float(data['chart']['result'][0]['meta']['previousClose'])
         variacao = preco_atual/ultimo_fechamento - 1
@@ -47,7 +47,7 @@ async def on_message(message):
             mensagem = f'Preço: R${preco_atual:.2f} - Variação: {variacao:.2%} Não houve variação de preço nessa ação até o momento.'
         await message.channel.send(mensagem)
     elif message.content.startswith('$acoesalta'):
-        data = json.loads(requests.get(URL_ALTA_BAIXA))
+        data = requests.get(URL_ALTA_BAIXA).json()
         mensagem = ''
         for i in range(5):
           acao = str(data['high'][i]['StockCode'])
@@ -56,7 +56,7 @@ async def on_message(message):
           mensagem += f'Ação: {acao}     Preço atual: R$ {preco:.2f} Variação: {oscilacao:.2f}%:chart_with_upwards_trend:\n'
         await message.channel.send(mensagem)
     elif message.content.startswith('$acoesbaixa'):
-        data = json.loads(requests.get(URL_ALTA_BAIXA))
+        data = requests.get(URL_ALTA_BAIXA).json()
         mensagem = ''
         for i in range(5):
           acao = str(data['low'][i]['StockCode'])
