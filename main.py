fb9ecbe21a0707d51d6a09de8dc7bbdd4e25409c
MrTaiko314 <glucio314@gmail.com>
2021-04-13 21:17:25 -0300

    Melhora na organização; Correção do bug na formatação do preço

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-import discord
 import os
-import schedule
 import time
-import tasks
 import json
-import requests
 import random
+
+import discord
+import schedule
+import tasks
+import requests
+
 from keep_alive import keep_alive
 
 client = discord.Client()
-urlaltabaixa = 'https://www.infomoney.com.br/wp-json/infomoney/v1/highlow'
+URL_ALTA_BAIXA = 'https://www.infomoney.com.br/wp-json/infomoney/v1/highlow'
 
 @client.event
 async def on_ready():
@@ -19,63 +21,49 @@ async def on_ready():
 async def on_message(message):  
     if message.author == client.user:
         return
-    if ('cogna' in message.content):
-        channel = message.channel
-        mensagem = ("COGNA É 15")
+
+    if 'cogna' in message.content.lower():
+        await message.channel.send('COGNA É 15')
+    
     if message.content.startswith('$google'):
-        mensagemDoUsuario = message.content
-        urlformatada = (mensagemDoUsuario[8:])
-        urlreplacada = urlformatada.replace(" ", "+")
-        urlGoogle = 'https://www.google.com/search?q=' + urlreplacada
-        channel = message.channel
-        await channel.send(urlGoogle)
-    if message.content.startswith('$acao'):
-        mensagemDoUsuario = message.content
-        msgformatada = (mensagemDoUsuario[6:])
+        mensagem_do_usuario = message.content
+        url_formatada = mensagem_do_usuario[8:]
+        url_replacada = url_formatada.replace(' ', '+')
+        url_google = 'https://www.google.com/search?q=' + url_replacada
+        await message.channel.send(url_google)
+    elif message.content.startswith('$acao'):
+        mensagem_do_usuario = message.content
+        msgformatada = mensagem_do_usuario[6:]
         url = 'https://query1.finance.yahoo.com/v8/finance/chart/' + msgformatada + '.SA?region=US&lang=en-US&includePrePost=false&interval=2m&useYfid=true&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance'
-        data = (requests.get(url))
-        precoAgora = float(
-            (data.json()['chart']['result'][0]['meta']['regularMarketPrice']))
-        precoAntesDeFechar = float(
-            (data.json()['chart']['result'][0]['meta']['previousClose']))
-        variacao = float((precoAgora / precoAntesDeFechar) - 1)
-        if (variacao < 0):
-            mensagem = ('Preço: R$' + str(precoAgora) + ' - Variação: ' +
-                        "{:.2%}".format(variacao) +
-                        ':chart_with_downwards_trend:')
-        elif (variacao > 0):
-            mensagem = ('Preço: R$' + str(precoAgora) + ' - Variação: ' +
-                        "{:.2%}".format(variacao) +
-                        ':chart_with_upwards_trend:')
+        data = json.loads(requests.get(url))
+        preco_atual = float(data['chart']['result'][0]['meta']['regularMarketPrice'])
+        ultimo_fechamento = float(data['chart']['result'][0]['meta']['previousClose'])
+        variacao = preco_atual/ultimo_fechamento - 1
+        if variacao < 0:
+            mensagem = f'Preço: R${preco_atual:.2f} - Variação: {variacao:.2%}:chart_with_downwards_trend:'
+        elif variacao > 0:
+            mensagem = f'Preço: R${preco_atual:.2f} - Variação: {variacao:.2%}:chart_with_upwards_trend:'
         else:
-            mensagem = (
-                'Preço: R$' + str(precoAgora) + ' - Variação: ' +
-                "{:.2%}".format(variacao) +
-                ' Não houve variação de preço nessa ação até o momento.')
-        channel = message.channel
-    if message.content.startswith('$acoesalta'):
-        channel = message.channel
-        data = (requests.get(urlaltabaixa))
-        i = 0
+            mensagem = f'Preço: R${preco_atual:.2f} - Variação: {variacao:.2%} Não houve variação de preço nessa ação até o momento.'
+        await message.channel.send(mensagem)
+    elif message.content.startswith('$acoesalta'):
+        data = json.loads(requests.get(URL_ALTA_BAIXA))
         mensagem = ''
-        while (i < 5 ):
-          acao = str(data.json()['high'][i]['StockCode'])
-          preco = str(data.json()['high'][i]['VALOR'])
-          oscilacao = float(data.json()['high'][i]['OSCILACAO'])/100
-          mensagem = mensagem + str(('Ação: '  +acao+ '     Preço atual: R$ '+str(preco)) + ' Variação: ' + "{:.2%}".format(oscilacao) + ':chart_with_upwards_trend:' '\n')
-          i = i + 1
-    if message.content.startswith('$acoesbaixa'):
-        channel = message.channel
-        data = (requests.get(urlaltabaixa))
-        i = 0
+        for i in range(5):
+          acao = str(data['high'][i]['StockCode'])
+          preco = float(data['high'][i]['VALOR'])
+          oscilacao = float(data['high'][i]['OSCILACAO'])
+          mensagem += f'Ação: {acao}     Preço atual: R$ {preco:.2f} Variação: {oscilacao:.2f}%:chart_with_upwards_trend:\n'
+        await message.channel.send(mensagem)
+    elif message.content.startswith('$acoesbaixa'):
+        data = json.loads(requests.get(URL_ALTA_BAIXA))
         mensagem = ''
-        while (i < 5 ):
-          acao = str(data.json()['low'][i]['StockCode'])
-          preco = str(data.json()['low'][i]['VALOR'])
-          oscilacao = float(data.json()['low'][i]['OSCILACAO'])/100
-          mensagem = mensagem + str(('Ação: '  +acao+ '     Preço atual: R$ '+str(preco)) + ' Variação: ' + "{:.2%}".format(oscilacao) + ':chart_with_downwards_trend:' '\n')
-          i = i + 1
-    await channel.send(mensagem)
+        for i in range(5):
+          acao = str(data['low'][i]['StockCode'])
+          preco = float(data['low'][i]['VALOR'])
+          oscilacao = float(data['low'][i]['OSCILACAO'])
+          mensagem += f'Ação: {acao}     Preço atual: R$ {preco:.2f} Variação: {oscilacao:.2f}%:chart_with_downwards_trend:\n'
+        await message.channel.send(mensagem)
 
 keep_alive()
 client.run(os.getenv('keybot'))
